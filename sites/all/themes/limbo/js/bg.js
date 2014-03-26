2cd1aeba01fd404161143acfdc720e7aad39934b
Chris Morris <cmorris@ccistudios.com>
2014-03-26 12:58:55 -0400

    Added landing page and imagemagick module

--- sites/all/themes/limbo/js/bg.js
+++ sites/all/themes/limbo/js/bg.js
@@ -1,10 +1,10 @@
 var header_image = 1;
 (function($) {
 	$(function(){
-		if ($("body").hasClass("front"))
+		if ($("body").hasClass("home"))
 		{
 			next();
-			setInterval(next, 8000);
+			setInterval(next, 6000);
 		}
 		else
 		{
--- sites/all/themes/limbo/js/instagram.js
+++ sites/all/themes/limbo/js/instagram.js
@@ -2,7 +2,7 @@
 	$(function(){
 		$.ajax({
 			"type":"GET",
-			"url":"https://api.instagram.com/v1/users/578209098/media/recent?count=1&client_id=78088912e63e4e4f9e573f5dd552bbf1",
+			"url":"https://api.instagram.com/v1/users/578209098/media/recent?count=4&client_id=78088912e63e4e4f9e573f5dd552bbf1",
 			"success":success,
 			"dataType":"jsonp"
 		});
@@ -10,10 +10,17 @@
 
 	function success(json)
 	{
-		if (json && json.data[0] && json.data[0].images && json.data[0].images.low_resolution && json.data[0].images.low_resolution.url)
+		if (json && json.data)
 		{
-			var url = json.data[0].images.low_resolution.url;
-			$("#block-block-5 img").attr("src", url);
+			for (var i=0; i<4; i++)
+			{
+				if (json.data[i] && json.data[i].images && json.data[i].images.low_resolution && json.data[i].images.low_resolution.url)
+				{
+					$("#block-block-5 .content a img.default").remove();
+					var url = json.data[i].images.low_resolution.url;
+					$("#block-block-5 .content a").append("<img src='"+url+"' />");
+				}
+			}
 		}
 	}
 }(jQuery));
