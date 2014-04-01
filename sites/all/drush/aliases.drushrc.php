dab586e24fc96703ee169f7b3eb39bce15ffbc54
Chris Morris <cmorris@ccistudios.com>
2014-04-01 09:57:45 -0400

    Fixes for the weekly features and the landing page

--- sites/all/drush/aliases.drushrc.php
+++ sites/all/drush/aliases.drushrc.php
@@ -11,3 +11,14 @@ $aliases['dev'] = array(
 	'ssh-options'=>'-p 37241'
 );
 
+
+$aliases['live'] = array(
+	'uri'=> 'live.limbolounge.ca',
+	'root' => '/home/limbo/subdomains/live/public_html',
+	'remote-host'=> 'host.lambtonshield.com',
+	'remote-user'=> 'limbo',
+	'path-aliases'=> array(
+		'%files'=> 'sites/default/files',
+	)
+);
+
